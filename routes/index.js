7c3f7b2cf26cbcff41f5429154bf85d8245dc8cf
slyvenegas <venegas@gmail.com>
2024-08-01 20:16:22 -0500

    vercel deploy

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,8 @@ const updateUser = require('../controller/user/updateUser');
 const UploadProductController = require('../controller/product/uploadProduct');
 const getProductController = require('../controller/product/getProduct');
 const updateProductController = require('../controller/product/updateProduct');
-const getCategoryProduct = require('../controller/product/getCategoryProduct');
+const getCategoryProduct = require('../controller/product/getCategoryProductOne');
+const getCategoryWiseProduct = require('../controller/product/getCategoryWiseProduct');
 
 
 
@@ -22,6 +23,7 @@ router.post('/signup', userSignUpController);
 router.post('/signin', userSignInController);
 router.get('/user-details',authToken,userDetailsController);
 router.get('/userLogout',userLogout);
+router.get('/api',(req, res) => res.send("Express on Vercelalejo here0"));
 
 
 
@@ -35,6 +37,7 @@ router.post("/upload-product",authToken,UploadProductController)
 router.get("/get-product",getProductController)
 router.post("/update-product",authToken,updateProductController)
 router.get("/get-categoryProduct",getCategoryProduct)
+router.post("/category-product",getCategoryWiseProduct)
 
 
 
