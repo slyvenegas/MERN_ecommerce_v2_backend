810168039bd4ffe1a6bf583796114fff94376a2b
slyvenegas <venegas@gmail.com>
2024-09-19 11:44:13 -0500

    prueba express en vercel

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cookieParser())
 
 app.use("/api",router)
 
-const PORT = 3000 || process.env.PORT
+const PORT = 8080 || process.env.PORT
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
@@ -25,3 +25,8 @@ connectDB().then(()=>{
 })
 
 
+// El archivo index.js es el punto de entrada
+// para tu aplicación backend.
+//  Aquí configuras el servidor, las dependencias
+// necesarias, y haces que tu aplicación esté
+// lista para aceptar peticiones.
