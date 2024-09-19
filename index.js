6bf6f67dbb65e151a9e3126e462218cab3f10e40
slyvenegas <venegas@gmail.com>
2024-09-19 12:38:39 -0500

    prueba express en vercel - v5

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cookieParser())
 
 app.use("/api",router)
 
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
